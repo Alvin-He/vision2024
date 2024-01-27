e071590fc1eee14f0e58b8d90aa9d08f674e5528
Alvin-He <shinypeach36@gmail.com>
2024-01-27 12:59:15 -0800

    nvmpi version

--- src/stream.py
+++ src/stream.py
@@ -1,4 +1,6 @@
 
+GUI=False
+
 import ffmpeg
 import subprocess as sp
 import cv2
@@ -6,11 +8,16 @@ import numpy as np
 import time
 cap = cv2.VideoCapture(0)
 
+kTargetFPS=10
+kIMG_WIDTH=640
+kIMG_HEIGHT=480
+
 # process = sp.Popen(ffmpeg_command, stdin=sp.PIPE)
 _stream = (ffmpeg
-    .input('pipe:', framerate='{}'.format(cap.get(cv2.CAP_PROP_FPS)), format='rawvideo', pix_fmt='bgr24', s='{}x{}'.format(int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
-    .output('rtsp://0.0.0.0:8554/stream', format='rtsp', pix_fmt='yuv420p', rtsp_transport='udp', vcodec='h264_nvenc', preset="p1", zerolatency='1',delay="0", bf='0', **{"profile:v":"baseline"})
-)
+    .input('pipe:', framerate=str(kTargetFPS), format='rawvideo', pix_fmt='bgr24', s='{}x{}'.format(int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+    .crop(0, 0, 640, 480)
+    .output('rtsp://0.0.0.0:8554/stream', format='rtsp', pix_fmt='yuv420p', rtsp_transport='udp', vcodec='h264_nvmpi', preset="ultrafast") #, bf='0', **{"profile:v":"baseline"}
+) 
 print("FFmpeg args: ", ffmpeg.get_args(_stream))
 process = (
     _stream
@@ -20,6 +27,10 @@ process = (
 
 while cap.isOpened():
     ret, img = cap.read()
+
+    #resize 
+    # img = cv2.cuda.resize(img,(kIMG_WIDTH, kIMG_HEIGHT),interpolation=cv2.INTER_LINEAR)
+
     t = 'time:' + str(time.time())
     cv2.putText(img,t,(0,15), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0, 0),1)
     # ffmepg x264 encoding 
@@ -31,6 +42,9 @@ while cap.isOpened():
     process.stdin.flush()
     # insert compression 
     # send net work time
+    if not GUI:
+        time.sleep((1/kTargetFPS) - (0.1*(1/kTargetFPS))) 
+        continue
     cv2.imshow("img", img)
     a = cv2.waitKey(20)
     if a == ord('q'): 
