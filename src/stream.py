e8bd7fafc6835942ff9ee0815024ad089ce65791
Alvin-He <shinypeach36@gmail.com>
2024-01-27 16:32:04 -0800

    I guess we have opencv streaming now

--- src/stream.py
+++ src/stream.py
@@ -8,15 +8,15 @@ import numpy as np
 import time
 cap = cv2.VideoCapture(0)
 
-kTargetFPS=10
+kTargetFPS=30
 kIMG_WIDTH=640
 kIMG_HEIGHT=480
 
 # process = sp.Popen(ffmpeg_command, stdin=sp.PIPE)
+#s='{}x{}'.format(int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))) s='{}x{}'.format(kIMG_WIDTH, kIMG_HEIGHT)
 _stream = (ffmpeg
-    .input('pipe:', framerate=str(kTargetFPS), format='rawvideo', pix_fmt='bgr24', s='{}x{}'.format(int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
-    .crop(0, 0, 640, 480)
-    .output('rtsp://0.0.0.0:8554/stream', format='rtsp', pix_fmt='yuv420p', rtsp_transport='udp', vcodec='h264_nvmpi', preset="ultrafast") #, bf='0', **{"profile:v":"baseline"}
+    .input('pipe:', framerate=str(kTargetFPS), format='rawvideo', pix_fmt='bgr24', s='{}x{}'.format(kIMG_WIDTH, kIMG_HEIGHT))
+    .output('rtsp://0.0.0.0:8554/stream', flags2="fast", format='rtsp', pix_fmt='yuv420p', rtsp_transport='udp', vcodec='h264_nvmpi', rc="cbr", preset="ultrafast") #, bf='0', **{"profile:v":"baseline"}
 ) 
 print("FFmpeg args: ", ffmpeg.get_args(_stream))
 process = (
@@ -25,15 +25,14 @@ process = (
     .run_async(pipe_stdin=True)
 )
 
-cuda_img_buf = cv2.cuda.GpuMat()
 while cap.isOpened():
     ret, img = cap.read()
-
     #resize 
+    cuda_img_buf = cv2.cuda_GpuMat()
     cuda_img_buf.upload(img)
-    cuda_img_buf = cv2.cuda.resize(cuda_img_buf, (640, 480))
+    retBuf = cv2.cuda.resize(cuda_img_buf, (640, 480), interpolation=cv2.INTER_LINEAR)
 
-    img = cuda_img_buf.download()
+    img = retBuf.download()
 
     t = 'time:' + str(time.time())
     cv2.putText(img,t,(0,15), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0, 0),1)
@@ -47,7 +46,7 @@ while cap.isOpened():
     # insert compression 
     # send net work time
     if not GUI:
-        time.sleep((1/kTargetFPS) - (0.1*(1/kTargetFPS))) 
+        # time.sleep((1/kTargetFPS) - (0.1*(1/kTargetFPS))) 
         continue
     cv2.imshow("img", img)
     a = cv2.waitKey(20)
